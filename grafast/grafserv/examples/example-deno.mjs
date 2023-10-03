470c56e05037d9c30d25d832792ead5d5785150f
Benjie Gillam <benjie@jemjie.com>
2023-10-03 16:21:28 +0100

    Some concessions

---
+++ grafast/grafserv/examples/example-deno.mjs
@@ -0,0 +1,9 @@
+import { grafserv } from "npm:grafserv/deno/v1";
+import preset from "./graphile.config.mjs";
+import schema from "./schema.mjs";
+
+// Create a Grafserv instance
+const serv = grafserv({ schema, preset });
+
+// Create a Deno server with our handler.
+Deno.serve({ port: preset.grafserv.port ?? 5678 }, serv.handler);
